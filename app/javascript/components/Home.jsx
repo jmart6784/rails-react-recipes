8827ea3e3f5b2dc8fbb3d772f134d248bc4251b0
jmart6784 <jmartinez6784@yahoo.com>
2021-04-02 22:43:59 -0700

    Added Recipe model and styled home page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div>
-    <h1>Food Recipes</h1>
-    <p>A curated list of recipes for the best homemade meal and delicacies.</p>
-    <Link to="/recipes" role="button">
-      View Recipes
-    </Link>
+  <div className="home-container">
+    <div className="home-mid-div">
+      <h1 className="home-title">Food Recipes</h1>
+      <p>
+        A curated list of recipes for the best homemade meal and delicacies.
+      </p>
+      <Link to="/recipes" className="home-btn-link" role="button">
+        View Recipes
+      </Link>
+    </div>
   </div>
 );
